39bbb72706bf8f0d68d44560ec4b6d5026a17bec
Manan <mananvala15@gmail.com>
2025-12-01 19:38:15 +0530

    deployment changes

--- rockophonix/settings.py
+++ rockophonix/settings.py
@@ -135,11 +135,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / 'static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
